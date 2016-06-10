5068cc1bf6561b0d4830714b0b049523c38d0e85
Marnit <githubmarnit@gmail.com>
2016-06-10 18:00:54 +0200

    Version 1.0.0
    First release

---
+++ CustomScenery/Main.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using MiniJSON;
+
+namespace Custom_Scenery
+{
+    public class Main : IMod, IModSettings
+    {
+		private StreamWriter sw;
+        private GameObject _go;
+		private Dictionary<string, object> car_settings;
+		private Dictionary<string, object> car_strings;
+		private Dictionary<string, string> settings_string = new Dictionary<string, string>();
+		public Dictionary<string, bool> settings_bool = new Dictionary<string, bool>();
+		private Type type;
+		private bool isenabled = false;
+		private string output;
+		private int i;
+		private int result;
+		private bool hotkey_rebind = false;
+
+		public void Init() {
+			car_settings = Json.Deserialize(File.ReadAllText(Path + @"/settings.json")) as Dictionary<string, object>;
+			car_strings = Json.Deserialize(File.ReadAllText(Path + @"/dictionary.json")) as Dictionary<string, object>;
+			foreach (KeyValuePair<string, object> S in car_settings) {
+				type = S.Value.GetType();
+				if(type==typeof(bool)) {
+					settings_bool[S.Key] = bool.Parse(S.Value.ToString());
+				} else {
+					settings_string[S.Key] = S.Value.ToString();
+				}
+			}
+		}
+		
+        public void onEnabled()
+        {
+			isenabled = true;
+			
+            _go = new GameObject();
+
+            _go.AddComponent<SceneryLoader>();
+
+            _go.GetComponent<SceneryLoader>().Path = Path;
+			
+            _go.GetComponent<SceneryLoader>().Settings = car_settings;
+
+            _go.GetComponent<SceneryLoader>().Identifier = Identifier;
+
+            _go.GetComponent<SceneryLoader>().LoadScenery();
+        }
+
+        public void onDisabled()
+        {
+			isenabled = false;
+			
+            _go.GetComponent<SceneryLoader>().UnloadScenery();
+
+            UnityEngine.Object.Destroy(_go);
+        }
+		
+		public void onDrawSettingsUI()
+		{
+			GUIStyle labelStyle = new GUIStyle (GUI.skin.label); 
+			labelStyle.margin=new RectOffset(15,0,10,0);
+			labelStyle.alignment = TextAnchor.MiddleLeft;
+			GUIStyle toggleStyle = new GUIStyle (GUI.skin.toggle); 
+			toggleStyle.margin=new RectOffset(0,10,19,24);
+			toggleStyle.alignment = TextAnchor.MiddleLeft;
+			GUIStyle textfieldStyle = new GUIStyle (GUI.skin.textField); 
+			textfieldStyle.margin=new RectOffset(0,10,10,0);
+			textfieldStyle.alignment = TextAnchor.MiddleCenter;
+			GUILayout.BeginHorizontal();
+			GUILayout.BeginVertical();
+			foreach (KeyValuePair<string, object> S in car_settings) {
+				GUILayout.Label (car_strings.ContainsKey(S.Key)?car_strings[S.Key].ToString():S.Key, labelStyle, GUILayout.Height(30));
+			}
+			GUILayout.EndVertical();
+			GUILayout.FlexibleSpace();
+			GUILayout.BeginVertical();
+			foreach (KeyValuePair<string, object> S in car_settings) {
+				type = S.Value.GetType();
+				if(type==typeof(bool)) {
+					settings_bool[S.Key] = GUILayout.Toggle (settings_bool[S.Key],"",toggleStyle, GUILayout.Width(16), GUILayout.Height(16));
+				} else {
+					settings_string[S.Key] = GUILayout.TextField (settings_string[S.Key], textfieldStyle, GUILayout.Width(130), GUILayout.Height(30));
+				}
+			}
+			GUILayout.EndVertical();
+			GUILayout.EndHorizontal();
+		}
+		
+		public void onSettingsOpened()
+		{
+
+		}
+		
+		public void onSettingsClosed()
+		{
+			writeSettingsFile();
+		}
+		
+		public void writeSettingsFile()
+		{
+			sw = File.CreateText(Path+@"/settings.json");
+			sw.WriteLine("{");
+			i = 0;
+			foreach (KeyValuePair<string, object> S in car_settings) {
+				type = S.Value.GetType();
+				i++;
+				output = "	\""+S.Key+"\": ";
+				if(type==typeof(bool)) {
+					output += settings_bool[S.Key].ToString().ToLower();
+				} else if(type==typeof(double)) {
+					output += settings_string[S.Key];
+					if(int.TryParse(settings_string[S.Key],out result)) {
+						output += ".0";
+					}
+				} else if(type==typeof(string)) {
+					output += "\""+settings_string[S.Key]+"\"";
+				}
+				if(i!=car_settings.Count){
+					output += ",";
+				}
+				sw.WriteLine(output);
+			}
+			sw.WriteLine("}");
+			sw.Flush();
+			sw.Close();	
+			Init();
+			if(isenabled==true) {
+				onDisabled();
+				onEnabled();
+			}
+		}
+
+        public string Name { get { return "Cars 'R' Us [v1.0.0]"; } }
+        public string Description { get { return "Decorative car props."; } }
+        private string path;
+		public string Path {
+			get {
+				return path;
+			}
+			set {
+				path = value;
+				Init();
+			}
+		}
+		
+        public string Identifier { get; set; }
+		
+		public void WriteToFile(string text) {
+			sw = File.AppendText(Path+@"/mod.log");
+			sw.WriteLine(text);
+			sw.Flush();
+			sw.Close();
+		}
+    }
+}
---
+++ CustomScenery/SceneryLoader.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Custom_Scenery.CustomScenery;
+using Custom_Scenery.CustomScenery.Decorators;
+using Custom_Scenery.Decorators;
+using MiniJSON;
+using UnityEngine;
+
+namespace Custom_Scenery
+{
+    internal class SceneryLoader : MonoBehaviour
+    {
+        private List<BuildableObject> _sceneryObjects = new List<BuildableObject>();
+
+        public string Path;
+		
+        public Dictionary<string, object> Settings;
+
+        public string Identifier;
+
+        public void LoadScenery()
+        {
+            try
+            {
+                var dict = Json.Deserialize(File.ReadAllText(Path + @"/scenery.json")) as Dictionary<string, object>;
+				GameObject hider = new GameObject();
+
+                char dsc = System.IO.Path.DirectorySeparatorChar;
+
+                using (WWW www = new WWW("file://" + Path + dsc + "assetbundle" + dsc + "scenery"))
+                {
+                    if (www.error != null)
+                        throw new Exception("Loading had an error:" + www.error);
+
+                    AssetBundle bundle = www.assetBundle;
+
+                    foreach (KeyValuePair<string, object> pair in dict)
+                    {
+                        try
+                        {
+							var options = pair.Value as Dictionary<string, object>;
+							if((bool)Settings["enableFlippedReliant"]==true||!options.ContainsKey("isFlippedReliant")) {
+								string mycategory = "";
+								if((bool)Settings["carPackGroup"]==true) {
+									mycategory += "Cars 'R' Us";
+								}
+								if((bool)Settings["seperateClass"]==true) {
+									if((string)options["class"]=="V") {
+										mycategory += (mycategory!=""?"/":"")+"Vehicles";
+									}
+								}
+								if((bool)Settings["seperateGroup"]==true) {
+									if((string)options["group"]=="C") {
+										mycategory += (mycategory!=""?"/":"")+"Cars";
+									}
+								}
+								if((bool)Settings["seperateBrand"]==true) {
+									if((string)options["brand"]=="CI") {
+										mycategory += (mycategory!=""?"/":"")+"Citroën";
+									}
+									if((string)options["brand"]=="VW") {
+										mycategory += (mycategory!=""?"/":"")+"Volkswagen";
+									}
+									if((string)options["brand"]=="TS") {
+										mycategory += (mycategory!=""?"/":"")+"Tesla";
+									}
+									if((string)options["brand"]=="CH") {
+										mycategory += (mycategory!=""?"/":"")+"Chevrolet";
+									}
+									if((string)options["brand"]=="RL") {
+										mycategory += (mycategory!=""?"/":"")+"Reliant";
+									}
+								}
+								if((bool)Settings["seperateCarType"]==true) {
+									if((string)options["cartype"]=="ST") {
+										mycategory += (mycategory!=""?"/":"")+"Station Wagon";
+									}
+									if((string)options["cartype"]=="SD") {
+										mycategory += (mycategory!=""?"/":"")+"Sedan";
+									}
+									if((string)options["cartype"]=="EX") {
+										mycategory += (mycategory!=""?"/":"")+"Executive";
+									}
+									if((string)options["cartype"]=="PC") {
+										mycategory += (mycategory!=""?"/":"")+"Pony Car";
+									}
+									if((string)options["cartype"]=="CC") {
+										mycategory += (mycategory!=""?"/":"")+"City Car";
+									}
+								}
+								GameObject asset = (new TypeDecorator((string)options["type"])).Decorate(options, bundle);
+								asset.GetComponent<BuildableObject>().categoryTag = mycategory;
+								(new PriceDecorator((double)options["price"])).Decorate(asset, options, bundle);
+								(new NameDecorator(pair.Key)).Decorate(asset, options, bundle);
+
+								if (options.ContainsKey("grid"))
+									(new GridDecorator((bool)options["grid"])).Decorate(asset, options, bundle);
+								
+								if (options.ContainsKey("recolorable"))
+									(new RecolorableDecorator((bool)options["recolorable"])).Decorate(asset, options, bundle);
+
+								DontDestroyOnLoad(asset);
+
+								AssetManager.Instance.registerObject(asset.GetComponent<BuildableObject>());
+								_sceneryObjects.Add(asset.GetComponent<BuildableObject>());
+								
+								// hide it from view
+								asset.transform.parent = hider.transform;
+							}
+						}
+                        catch (Exception e)
+                        {
+                            Debug.Log(e);
+
+                            LogException(e);
+                        }
+                    }
+
+                    bundle.Unload(false);
+                }
+
+                hider.SetActive(false);
+            }
+            catch(Exception e)
+            {
+                LogException(e);
+            }
+        }
+
+        private void LogException(Exception e)
+        {
+            StreamWriter sw = File.AppendText(Path + @"/mod.log");
+
+            sw.WriteLine(e);
+
+            sw.Flush();
+
+            sw.Close();
+        }
+
+        public void UnloadScenery()
+        {
+            foreach (BuildableObject deco in _sceneryObjects)
+            {
+                AssetManager.Instance.unregisterObject(deco);
+            }
+        }
+    }
+}
